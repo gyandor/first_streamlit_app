222c088df13a25c8b6b7d4a982f31eeb4507697e
gyandor <48220282+gyandor@users.noreply.github.com>
2022-07-06 08:54:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
-streamlit.markdown('Omega 3 & Blueberry Oatmeal')
-streamlit.markdown('Kale, Spinach & Rocket Smoothie')
-streamlit.markdown('Hard-Boiled Free-Range Egg' )
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg' )
